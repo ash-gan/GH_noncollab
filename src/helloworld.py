5fae60ba01106a45d202545bdf263c46f015074d
Asha Gandhudi <agandhudi@gmail.com>
2022-06-02 21:59:04 -0700

    push changes to second_feature branch

--- src/helloworld.py
+++ src/helloworld.py
@@ -2,8 +2,13 @@ def hello_world(city):
     print(f'Hello World from {city}')
 
 
+def new_function(name):
+    print("My name is: ", name)
+
+
 def main():
     hello_world('NYC')
+    new_function("Asha")
 
 
 main()
