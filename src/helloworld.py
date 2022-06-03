5490a7716e3c9563232ac4d868fee5827e26a040
Asha Gandhudi <agandhudi@gmail.com>
2022-06-02 22:40:36 -0700

    first_commit_checkin

--- src/helloworld.py
+++ src/helloworld.py
@@ -13,10 +13,15 @@ def another_func(branch):
     print(branch)
 
 
+def first_commit(my_name):
+    print(my_name)
+
+
 def main():
     hello_world('NYC')
     new_function("Asha")
     another_func("new_feature")
+    first_commit("call me")
 
 
 main()
